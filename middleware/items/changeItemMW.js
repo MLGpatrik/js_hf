3a8cbfbd2e09c7e24a4fad4a9d12845dacdf928d
pmayor <patrikmayor@gmail.com>
2022-05-03 16:16:31 +0200

    HF6 bugfix

--- middleware/items/changeItemMW.js
+++ middleware/items/changeItemMW.js
@@ -19,7 +19,7 @@ module.exports = function (objectrepository) {
         }
 
         console.log("Editing item: \n item id: "+req.body.item_id+ " name: "+req.body.item_name+" item size: "+req.body.item_size+" price: "+req.body.item_price);
-        ItemModel.findOneAndUpdate({_id:req.body.item_id}, {itemName: req.body.item_name, size: req.body.item_size, price:req.body.item_price }, (err)=>{
+        ItemModel.findOneAndUpdate({_id:req.body.item_id}, {itemName: req.body.item_name, itemSize: req.body.item_size, price:req.body.item_price }, (err)=>{
             return res.end()
         });
     };
